ac4b1609f40588e14335114333bcea6de53ba1de
Bill Barnett <bill@agilo.us>
2009-11-14 01:42:47 -0500

    symlink shared files

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,21 +6,25 @@ set :application_dir,   "/home/#{user}/domains/#{application}.com"
 set :repository,        "git@github.com:agilous/justmissedme.git"
 set :scm,               :git
 
-set :deploy_to,         :application_dir
+set :deploy_to,         application_dir
 set :deploy_via,        :remote_cache
 
 ssh_options[:keys] =    %w(~/.ssh/id_rsa)
 set :chmod755,          "app config db lib public vendor script script/* public/disp*"
 set :use_sudo,          false
 
-role :app,              "bam.dreamhost.com"
-role :web,              "bam.dreamhost.com"
-role :db,               "db.justmissedme.com", :primary => true
+role :app,              domain
+role :web,              domain
+role :db,               domain, :primary => true
 
 namespace :deploy do
-  desc "Restarting mod_rails with restart.txt"
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :symlink_configs do
+    run "ln -nfs #{shared_path}/config/database.yml #{current_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/log/production.log #{current_path}/log/production.log"
   end
 
   [:start, :stop].each do |t|
