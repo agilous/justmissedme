32bc1e570e1c74fce3808c9261bde0307e52bf82
removedporn <86824510+removedporn@users.noreply.github.com>
2023-05-01 14:47:54 +0800

    Delete app directory

--- app/controllers/pages_controller.rb
+++
@@ -1,4 +0,0 @@
-class PagesController < ApplicationController
-  def faq
-  end
-end
--- app/models/person.rb
+++
@@ -1,5 +0,0 @@
-class Person < ActiveRecord::Base
-  validates :name, :presence => true
-  validates :page_url, :presence => true
-  validates :date_of_death, :presence => true
-end
